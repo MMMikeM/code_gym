3d2949db5d4b12c26899c7c81d0d024c682297b1
MMMike <mikem.tba@gmail.com>
2022-08-13 19:17:49 +0200

    feat: :sparkles: Update Fastify server

---
+++ server/fastify/src/server.ts
@@ -0,0 +1,40 @@
+// Read the .env file.
+import * as dotenv from "dotenv"
+dotenv.config()
+
+// Require the framework
+import Fastify from "fastify"
+
+import closeWithGrace from "close-with-grace"
+
+const app = Fastify({
+  logger: true,
+})
+
+// Register your application as a normal plugin.
+app.register(import("./app"))
+
+// delay is the number of milliseconds for the graceful close to finish
+const closeListeners = closeWithGrace({ delay: 500 }, async function ({
+  signal,
+  err,
+  manual,
+}) {
+  if (err) {
+    app.log.error(err)
+  }
+  await app.close()
+} as closeWithGrace.CloseWithGraceAsyncCallback)
+
+app.addHook("onClose", async (instance, done) => {
+  closeListeners.uninstall()
+  done()
+})
+
+// Start listening.
+app.listen({ port: parseInt(process.env.PORT) || 3000 }, (err: any) => {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+})
